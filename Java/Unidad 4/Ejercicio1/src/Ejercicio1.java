e4caac9f5ac57c0122c6bfd1e13da271cfb4e482
Franciscomr10 <fmarreg0806@g.educaand.es>
2023-11-02 18:58:46 +0100

    tercero

--- Java/Unidad 4/Ejercicio1/src/Ejercicio1.java
+++ Java/Unidad 4/Ejercicio1/src/Ejercicio1.java
@@ -1,5 +1,17 @@
-public class App {
+import java.util.Scanner;
+
+public class Ejercicio1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner s = new Scanner(System.in);
+        System.out.println("Introduce el día de la semana");
+        String dia = s.nextLine();
+        if ((dia.equals("Lunes")) || (dia.equals("Martes"))){
+            System.out.println("Sistemas Informáticos");
+        } else if ((dia.equals("Miercoles")) || (dia.equals("Viernes"))) {
+                System.out.println("Base de Datos");
+            } else if (dia.equals("Jueves")) {
+                    System.out.println("Lenguaje de Marcas");
+                }            s.close();
+
+            }
 }
