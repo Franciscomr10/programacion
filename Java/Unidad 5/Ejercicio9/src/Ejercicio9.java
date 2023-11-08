f7b7cd766bb221813db91d0bb12d598ab866f8e5
Franciscomr10 <fmarreg0806@g.educaand.es>
2023-11-08 19:46:31 +0100

    cuarto

--- Java/Unidad 5/Ejercicio9/src/Ejercicio9.java
+++ Java/Unidad 5/Ejercicio9/src/Ejercicio9.java
@@ -1,5 +1,5 @@
-public class Ejercicio20 {
+public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("H");
+        System.out.println("Hello, World!");
     }
 }
